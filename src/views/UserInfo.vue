00b4aad57ce50c264966be47910befba219f4e65
yale8848 <royal8848@163.com>
2019-03-05 20:48:42 +0800

    test build

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -1,11 +1,16 @@
 <template>
 	<div class="view-user-info">
-		user-info
+		user-info<br/>
+		<img src="../assets/woods.jpg"/>
 	</div>
 </template>
-<style>
+<style lang="scss">
 	.view-user-info{
 		text-align: center;
-		margin-top: 100px;;
+		margin-top: 100px;
+		
+		img {
+			max-width:500px;
+		}
 	}
 </style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/production-sub-path/'
+    : '/',
+	outputDir:'dist',
+	productionSourceMap:false
+}
