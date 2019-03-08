436ee4f33c33d96856c73901ebef1b6e62160c1d
yale8848 <royal8848@163.com>
2019-03-08 16:11:03 +0800

    add express lib

--- server.js
+++ server.js
@@ -2,10 +2,32 @@ var express = require('express');
 var app = express();
 var proxy = require('http-proxy-middleware');
 var fs = require('fs');
+var path = require('path');
+var helmet = require('helmet');
+var compression = require('compression')
+
+
+const PUBLIC_PATH='dist';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-app.use(express.static('dist'));
+let port = isProduction?9568:8000;
+
+
+app.use(helmet());
+app.use(compression());
+
+app.use(function (req,res,next) {
+	if(req.path==='/'){
+		res.send("not find");
+	}else{
+		next();
+	}
+},express.static(PUBLIC_PATH));
+
+app.get('/admin', function (req, res) {
+	 res.sendFile( path.join(__dirname,PUBLIC_PATH,'index.html'));
+});
 
 app.all('*', function (req, res, next) {
 	if(!isProduction){
@@ -47,7 +69,6 @@ var options = {
 }
 app.use('/web-prod/gank',proxy(options))
 
-let port = isProduction?9568:8000;
 app.listen(port, function () {
   console.log('server start at '+port);
 });
